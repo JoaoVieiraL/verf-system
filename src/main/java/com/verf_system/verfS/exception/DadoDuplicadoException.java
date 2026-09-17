dec698cb3d7365709f3d031490de29c6a8714f74
JoaoVieiraL <vieiralima1703@gmail.com>
2026-09-17 16:48:56 -0300

    feat(backend): adiciona exceções
    Cria DadoDuplicadoException, NaoEncontradoException e
    RegraDeNegocioException.

---
+++ src/main/java/com/verf_system/verfS/exception/DadoDuplicadoException.java
@@ -0,0 +1,7 @@
+package com.verf_system.verfS.exception;
+
+public class DadoDuplicadoException extends RuntimeException {
+    public DadoDuplicadoException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/com/verf_system/verfS/exception/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package com.verf_system.verfS.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String mensagem) {
+        super(mensagem);
+    }
+
+}
