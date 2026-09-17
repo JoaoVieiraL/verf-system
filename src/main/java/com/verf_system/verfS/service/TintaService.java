3642bf4e2eeb5d47243f395990db4a51368e0028
JoaoVieiraL <vieiralima1703@gmail.com>
2026-09-17 13:27:18 -0300

    fix(backend): permite cadastrar tinta produzida sem fornecedor

--- src/main/java/com/verf_system/verfS/service/TintaService.java
+++ src/main/java/com/verf_system/verfS/service/TintaService.java
@@ -1,6 +1,7 @@
 package com.verf_system.verfS.service;
 
 import com.verf_system.verfS.database.entity.FornecedorEntity;
+import com.verf_system.verfS.database.entity.OrigemTinta;
 import com.verf_system.verfS.database.entity.TintaEntity;
 import com.verf_system.verfS.database.repository.IFornecedorRepository;
 import com.verf_system.verfS.database.repository.ITintaRepository;
@@ -15,17 +16,25 @@ import java.util.List;
 public class TintaService {
     private final ITintaRepository tintaRepository;
     private final IFornecedorRepository fornecedorRepository;
+    FornecedorEntity fornecedor = null;
 
     public void save(TintaDto tintaDto) {
-        FornecedorEntity fornecedor = fornecedorRepository.findById(tintaDto.getIdFornecedor()).orElseThrow(()-> new RuntimeException("Nenhum Fornecedor encontrado"));
+        if(tintaDto.getOrigem() == OrigemTinta.COMPRADA) {
+            if(tintaDto.getIdFornecedor() == null) {
+                throw new RuntimeException ("Nenhum Fornecedor encontrado");
+            }
+            fornecedor = fornecedorRepository.findById(tintaDto.getIdFornecedor()).orElseThrow(() -> new RuntimeException("Nenhum Fornecedor encontrado"));
 
+        }else if(tintaDto.getIdFornecedor() != null) {
+            throw new RuntimeException ("Tinta Produzida nao deve ter Fornecedor");
+        }
         tintaRepository.save(TintaEntity.builder()
-                        .nome(tintaDto.getNome())
-                        .numeroHexadecimal(tintaDto.getNumeroHexadecimal())
-                        .codigo(tintaDto.getCodigo())
-                        .origemTinta(tintaDto.getOrigem())
-                        .ativo(tintaDto.isAtivo())
-                        .fornecedorRef(fornecedor)
+                .nome(tintaDto.getNome())
+                .numeroHexadecimal(tintaDto.getNumeroHexadecimal())
+                .codigo(tintaDto.getCodigo())
+                .origemTinta(tintaDto.getOrigem())
+                .ativo(tintaDto.isAtivo())
+                .fornecedorRef(fornecedor)
                 .build());
 
     }
