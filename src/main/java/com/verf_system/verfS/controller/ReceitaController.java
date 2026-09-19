01be5ee0860b3b322d56f80a1c171917c0bad101
JoãoVieiraL <vieiralima1703@gmail.com>
2026-09-19 17:52:38 -0300

    feat(backend): adiciona validação de dados para receitas
    - Adiciona @Valid no endpoint POST
    - Adiciona @NotNull, @Size  no dto

--- src/main/java/com/verf_system/verfS/controller/ReceitaController.java
+++ src/main/java/com/verf_system/verfS/controller/ReceitaController.java
@@ -3,6 +3,7 @@ package com.verf_system.verfS.controller;
 import com.verf_system.verfS.database.entity.ReceitaEntity;
 import com.verf_system.verfS.dto.ReceitaDto;
 import com.verf_system.verfS.service.ReceitaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class ReceitaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void save(@RequestBody ReceitaDto receitaDto) {
+    public void save(@Valid @RequestBody ReceitaDto receitaDto) {
         receitaService.save(receitaDto);
     }
 }
--- src/main/java/com/verf_system/verfS/dto/ReceitaDto.java
+++ src/main/java/com/verf_system/verfS/dto/ReceitaDto.java
@@ -1,5 +1,7 @@
 package com.verf_system.verfS.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -10,8 +12,14 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReceitaDto {
+
+    @NotNull
+    @Size(max = 50)
     private String nome;
+    @NotNull
     private Long idTintaResultante;
+    @NotNull
     private Long idCriadoPor;
+    @Size(max = 200)
     private BigDecimal valorPorLitro;
 }
